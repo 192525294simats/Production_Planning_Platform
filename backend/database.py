0fcadd161b85a90f6216f3f14ee5d563212f8bf1
Sivalanka Hasini <192525294.simats@saveetha.com>
2026-08-26 12:47:15 +0530

    Update production planning platform

--- backend/database.py
+++ backend/database.py
@@ -4,7 +4,7 @@ import psycopg2
 
 def get_connection():
     return psycopg2.connect(
-        host=os.getenv("DB_HOST", "localhost"),
+        host=os.getenv("DB_HOST", "factory-database"),
         database=os.getenv("DB_NAME", "factorydb"),
         user=os.getenv("DB_USER", "factoryuser"),
         password=os.getenv("DB_PASSWORD", "factorypass"),
